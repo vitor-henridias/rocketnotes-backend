2942d7810227cbbff0e05f9e5b302dbb41f5fc8b
vitor-henridias <vhsilva.mg@gmail.com>
2024-01-09 16:29:41 -0300

    wip

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('jest').Config} */
+
+const config = {
+  bail: true,
+  // clearMocks: true,
+  coverageProvider: "v8",
+
+  // testMatch: [
+  //   "<rootDir>/src/**/*.spec.js"
+  // ]
+};
+
+module.exports = config;
